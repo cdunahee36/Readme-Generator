91d0ebd0c8fe34af35b5c21a4b300203201ebb0e
cdunahee36 <dunahee10@gmail.com>
2020-11-24 16:23:08 -0600

    Created the questions for the user.

---
+++ index.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the Title of your application?',
+      name: 'Title',
+    },
+    {
+      type: 'input',
+      message: 'Please write a description of your application.',
+      name: 'Description',
+    },
+    {
+      type: 'input',
+      message: 'Please write a table of contents for your application.',
+      name: 'ToC',
+    },
+    {
+      type: 'input',
+      message: 'How do you install your application?',
+      name: 'Installation',
+    },
+    {
+      type: 'input',
+      message: 'What is your application used for?',
+      name: 'Usage',
+    },
+    {
+      type: 'input',
+      message: 'What is the license of your application?',
+      name: 'License',
+    },
+    {
+      type: 'input',
+      message: 'Who were the contributors of the application?',
+      name: 'Contributing',
+    },
+    {
+      type: 'input',
+      message: 'Please put the tests of the application here.',
+      name: 'Tests',
+    },
+    {
+      type: 'input',
+      message: 'Put any questions for the application here.',
+      name: 'Questions',
+    },
+
+  ])
+
+  .then((response) => {
+    console.log('response', response)
+  
+
+
+const userInput =
+    `
+  `;
+
+  fs.writeFile('README.md', userInput, (err) =>
+   err ? console.error(err) : console.log('You have created your personalized Readme file!')
+ );
+
+
+});
+
+
